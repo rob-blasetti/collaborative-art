d2218d668e70012687c6019eefbd8b5d4d27727a
Rob Blasetti <rblasett@thoughtworks.com>
2023-08-29 12:58:22 +1000

    update grid dimensions to work with back end

--- src/gridDimensions.js
+++ src/gridDimensions.js
@@ -1,26 +1,30 @@
 import { ref, get, set } from 'firebase/database';
 import firebase from './firebase'; 
 
-// Define a function to fetch grid dimensions from the database
+// Function to fetch grid dimensions from the database
 const fetchGridDimensions = async () => {
   const gridDimensionsRef = ref(firebase.database(), 'gridDimensions');
   try {
     const snapshot = await get(gridDimensionsRef);
-    const gridDimensions = snapshot.val();
-    return gridDimensions;
+    if (snapshot.exists()) {
+      return snapshot.val();
+    } else {
+      console.warn("No grid dimensions found in the database.");
+      return null;
+    }
   } catch (error) {
     console.error('Error fetching grid dimensions:', error);
     throw error;
   }
 };
 
-// Define a function to update grid dimensions in the database
+// Function to update grid dimensions in the database
 const updateGridDimensions = async (newRows, newCols) => {
   const gridDimensionsRef = ref(firebase.database(), 'gridDimensions');
-  const updatedGridDimensions = { rows: newRows, cols: newCols };
+  const updatedGridDimensions = { rows: parseInt(newRows, 10), cols: parseInt(newCols, 10) }; // Ensure the values are numbers
   try {
     await set(gridDimensionsRef, updatedGridDimensions);
-    console.log('Grid dimensions updated successfully');
+    console.log('Grid dimensions updated successfully:', updatedGridDimensions);
   } catch (error) {
     console.error('Error updating grid dimensions:', error);
     throw error;
